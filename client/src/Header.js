f2a9d8c468e5aa90423087713acc35e338dc9345
Suyash Vyas <suyashm23@gmail.com>
2023-03-11 14:01:35 +0530

    Added react heroicons, clickout detection for the user profile modal in navbar

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,6 +1,20 @@
 import Logo from './newsAggregatorLogo.png';
-import Avatar from './avatar.png';
+import { UserIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon }   from '@heroicons/react/24/solid';
+import LoginSignupButton from './LoginSignupButton.js'
+import {useState} from 'react';
+import ClickOutHandler from 'react-clickout-handler';
 function Header(){
+    const [userDropDownVisibilityClass, setUserDropDownVisibilityClass]= useState ('hidden');
+
+    function toggleUserDropDown() {
+        if(userDropDownVisibilityClass === 'hidden'){
+            setUserDropDownVisibilityClass('block');
+        }
+        else{
+            setUserDropDownVisibilityClass('hidden');
+        }
+    }
     return (
         <header className="w-full bg-dark p-2">
             <div className="mx-4 flex">
@@ -8,11 +22,22 @@ function Header(){
                 <form action="" className="bg-dark-brighter p-1 px-3 flex rounded-md border border-borderColor mx-4 flex-grow">
                     <input type="text" className="bg-dark-brighter p-1 pl-2 pr-0 text-sm block focus:outline-none text-white" placeholder="Search" />
                 </form>
-                <button className="rounded-md flex">
-                    <div className="w-8 h=8 bg-gray-600 rounded-md">
-                        <img src={Avatar} alt="" style={{ filter: 'invert(100%)' }} className="block w-8 h-8" />
-                    </div>
-                </button>
+                <div className="mx-3">
+                    <LoginSignupButton >Log In</LoginSignupButton>
+                    <LoginSignupButton >Sign Up</LoginSignupButton>
+                </div>
+
+                <ClickOutHandler onClickOut={()=> setUserDropDownVisibilityClass('hidden')}>
+                    <button className="rounded-md flex border border-gray-700" onClick={() => toggleUserDropDown()}>
+                        <UserIcon className="w-6 h-6 text-gray-400 m-1" />
+                        <ChevronDownIcon className="text-gray-500 w-5 h-5 mt-2 m-1" />
+                    </button>
+                </ClickOutHandler>
+                <div className={"absolute right-0 top-12 bg-dark border border-gray-700 z-10 rounded-md text-Text overflow-hidden " + userDropDownVisibilityClass} >
+                    <button href="" className="block flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm">
+                        Log In / Sign Up
+                    </button>
+                </div>
             </div>
 
         </header>
---
+++ client/src/LoginSignupButton.js
@@ -0,0 +1,6 @@
+function LoginSignupButton(props){
+    return (
+        <button className="border border-gray-300 bg-gray-300 text-dark rounded-full px-3 mr-1 text-sm font-bold h-8" {...props}/>
+    )
+}
+export default LoginSignupButton
