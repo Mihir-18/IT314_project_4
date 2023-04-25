f84e48979605ac6a3270ab6826f7c8a457b0c662
Dakshveer Singh Chauhan <dakshveersinghchauhan@gmail.com>
2023-04-26 01:57:57 +0530

    added comments and replies functionality

--- api/server.js
+++ api/server.js
@@ -114,10 +114,17 @@ app.post('/logout', (req, res) => {
 });
 
 app.get('/comments', (req, res)=>{
-     Comment.find().sort({postedAt: -1}).then(comments =>{
+     Comment.find({rootId:null}).sort({postedAt: -1}).then(comments =>{
           res.json(comments);
      });
 });
+
+app.get('/comments/root/:rootId', (req, res)=>{
+     Comment.find({rootId:req.params.rootId}).sort({postedAt: -1}).then(comments =>{
+          res.json(comments);
+     });
+});
+
 
 app.get('/comments/:id', (req, res)=>{
      Comment.findById(req.params.id).then(comment => {
@@ -132,8 +139,15 @@ app.post('/comments', (req, res)=>{
           return;
      }
      getUserFromToken(token).then(userInfo=>{
-          const { title, body } = req.body;
-          const comment = new Comment({ title, body, author:userInfo.username, postedAt: new Date(),});
+          const {title,body,parentId,rootId} = req.body;
+          const comment = new Comment({
+          title,
+          body,
+          author:userInfo.username,
+          postedAt:new Date(),
+          parentId,
+          rootId,
+          });
           comment.save().then(savedComment => {
                res.json(savedComment);
           }).catch(console.log);
--- client/src/CommentModal.js
+++ client/src/CommentModal.js
@@ -2,11 +2,12 @@ import PostContent from './PostContent.js';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import ClickOutHandler from 'react-clickout-handler';
+import CommentForm from './CommentForm.js';
+import Comments from './Comments.js';
 
 function CommentModal(props){
-
     const [comment, setComment] = useState({});
-
+    const [comments, setComments] = useState([]);
     const visibleClass= props.open? 'block': 'hidden';
 
     useEffect(() => {
@@ -15,6 +16,11 @@ function CommentModal(props){
                     setComment(response.data);
                 }
             );
+        axios.get('http://localhost:4000/comments/root/' + props.id)
+            .then(response => {
+                setComments(response.data);
+            });
+
     }, [props.id]);
 
     function close(){
@@ -28,6 +34,14 @@ function CommentModal(props){
                 <div className="border border-reddit_dark-brightest my-4 w-3/4 md:w-1/2 bg-reddit_dark text-reddit_text p-4 mx-auto self-center rounded-md ">
                     <div className="block overflow-scroll" style={{ maxHeight: "calc(100vh - 200px)" }}>
                         <PostContent open={true} {...comment} />
+                        {!!comment && !!comment._id && (
+                            <>
+                                <hr className="border-reddit_border my-4"/>
+                                <CommentForm rootId={comment._id} parentId={comment._id} showAuthor={true}/>
+                                <hr className="border-reddit_border my-4"/>
+                                <Comments parentId={comment._id} comments={comments} />
+                            </>
+                        )}
                     </div>
                 </div>
             </ClickOutHandler>  
