95183f71598b7a1582f93b4c19fa086ba5b2493a
VAGHANI18 <202001015@daiict.ac.in>
2023-04-27 18:54:00 +0530

    added timeago-react for time display of posts and comments

--- client/src/Comments.js
+++ client/src/Comments.js
@@ -5,6 +5,7 @@ import RootCommentContext from "./RootCommentContext.js";
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import Voting from './Voting.js';
+import TimeAgo from 'timeago-react'
 
 function Comments(props){
     const rootCommentInfo = useContext(RootCommentContext);
@@ -20,7 +21,7 @@ function Comments(props){
                     <div className="flex mb-2">
                         <div className="bg-reddit_text w-10 h-10 rounded-full mr-2"/>
                         <div className="leading-10 pr-2 text-lg font-sans">{comment.author}</div>
-                        <div className="leading-10 text-md font-sans">{comment.postedAt}</div>
+                        <TimeAgo className="leading-10 text-md font-sans" datetime={comment.postedAt} />
                     </div>
                     <div className="border-l-2 border-reddit_text-darker p-3 pb-0" style={{marginLeft:'18px'}}>
                         <div className="pl-4 -mt-4">
--- client/src/PostContent.js
+++ client/src/PostContent.js
@@ -1,10 +1,11 @@
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
+import TimeAgo from 'timeago-react'
 
 function PostContent(props) {
     return (
         <div>
-            <h5 className="text-reddit_text-darker text-sm mb-1 px-4">Posted by {props.author} {props.postedAt}</h5>
+            <h5 className="text-reddit_text-darker text-sm mb-1 px-4">Posted by {props.author} <TimeAgo datetime={props.postedAt}/></h5>
             <h2 className='text-xl mb-3 px-4'>{props.title}</h2>
             <div className='text-sm leading-6 px-6'>
                 <ReactMarkdown  plugins={[gfm]} children={props.body} />
